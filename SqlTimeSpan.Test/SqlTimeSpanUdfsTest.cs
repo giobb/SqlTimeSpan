92ed4965a54f47ae926c0e0b28c1b9ece50ac3e0
twisturtales <publiccodingx@gmail.com>
2021-06-03 22:32:53 -0700

    udf test in progress

---
+++ SqlTimeSpan.Test/SqlTimeSpanUdfsTest.cs
@@ -0,0 +1,24 @@
+﻿using static SLackerSLab.SqlFunctions.SqlTimeSpanUdfs;
+using Xunit;
+using System;
+using SLackerSLab.SqlTypes;
+
+namespace SLackerSLab.Test
+{
+    public class SqlTimeSpanUdfsTest
+    {
+        [Fact]
+        public void TSAddToDTTest()
+        {
+            var result = TSAddToDT(DateTime.Parse("1/1/2021 5:00 AM"), SqlTimeSpan.FromHours(1));
+            Assert.Equal(DateTime.Parse("1/1/2021 6:00 AM"), result);
+        }
+
+        [Fact]
+        public void TSDateDiffTest()
+        {
+            var result = TSDateDiff(DateTime.Parse("1/1/2021"), DateTime.Parse("1/2/2021 1:00 AM"));
+            Assert.Equal(SqlTimeSpan.FromHours(25), result);
+        }
+    }
+}
